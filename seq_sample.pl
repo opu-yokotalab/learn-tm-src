d914354ae055f6ffd820b1641d0a9fd5d38339d6
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-10-17 09:01:58 +0000

    修正
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@5 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seq_sample.pl
+++ seq_sample.pl
@@ -1,15 +1,16 @@
 # 学習シーケンシング　サンプル2
-next([start,intro],[]).
+next([start,intro],[[msg,"じゃあ、がんばって勉強しなさいよ。"]]).
 next([intro,grammar],[]).
 next([grammar,example],[]).
 next([example,IFtest],[]).
-next([IFtest,end],[[msg,"Congratulations!"]]):-testCompare(test_000,Point),Point>=60.
-next([IFtest,intro],[]):-testCompare(test_000,Point),Point<60.
+next([IFtest,end],[[msg,"満点ですね。よくできました。"]]):-testCompare(test_000,Point),80<=Point.
+next([IFtest,end],[[msg,"一応合格ですが、もっと上を目指しましょう。"]]):-testCompare(test_000,Point),60<=Point,Point<80.
+next([IFtest,intro],[[msg,"あんた、もっとしっかりせられえ。"]]):-testCompare(test_000,Point),Point < 60.
 
 toc(intro,[]).
 toc(grammar,[]).
 toc(example,[]).
 toc(IFtest,[]).
 
-changeLv([up,5],[]):-currentLevel(L),L=1.
-changeLv([down,1],[]):-currentLevel(L),L=5.
+changeLv([up,5],[]):-currentLevel(L),L==1.
+changeLv([down,1],[]):-currentLevel(L),L==5.
