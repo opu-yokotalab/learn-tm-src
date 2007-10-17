04e9baa8772527ad2a5c0d9c9aa2bb7e807c26e0
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-10-17 12:15:53 +0000

    saiki.pl add
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@17 139e5372-204a-47f5-a165-b1eaa1f9cd96

---
+++ seqs/seq_saiki.pl
@@ -0,0 +1,7 @@
+# 学習シーケンシング　再帰アルゴリズム
+next([start,saiki_intro],[[msg,"再帰アルゴリズムについて学習しましょう。"]]).
+next([saiki_intro,saiki_sample],[]).
+next([saiki_sample,end],[]).
+
+toc(saiki_intro,[]).
+toc(saiki_sample,[]).
