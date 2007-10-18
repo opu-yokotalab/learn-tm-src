b404157f2e3e43d06f691a36bf20338a20a20d4c
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-10-18 10:59:29 +0000

    再帰の教材 クイックソートまで作成
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@21 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seqs/seq_saiki.pl
+++ seqs/seq_saiki.pl
@@ -1,7 +1,10 @@
-# 学習シーケンシング　再帰アルゴリズム
 next([start,saiki_intro],[[msg,"再帰アルゴリズムについて学習しましょう。"]]).
-next([saiki_intro,saiki_sample],[]).
-next([saiki_sample,end],[]).
+next([saiki_intro,saiki_zenka],[]).
+next([saiki_zenka,saiki_fibo],[]).
+next([saiki_fibo,saiki_sample],[]).
+next([saiki_sample,end],[[msg,"お疲れ様!!"]]).
 
 toc(saiki_intro,[]).
+toc(saiki_zenka,[]).
+toc(saiki_fibo,[]).
 toc(saiki_sample,[]).
