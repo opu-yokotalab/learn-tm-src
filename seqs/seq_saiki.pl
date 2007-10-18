05dd6a40e7495a4638091df747d323a5a365eb06
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-10-18 16:33:55 +0000

    再帰　シーケンシング　微修正
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@27 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seqs/seq_saiki.pl
+++ seqs/seq_saiki.pl
@@ -1,13 +1,13 @@
 next([start,saiki_intro],[[msg,"再帰アルゴリズムについて学習しましょう。"]]).
 next([saiki_intro,saiki_zenka],[]).
-next([saiki_zenka,saiki_fibo],[[msg,"難しい方の教材によくぞ挑戦しました。感動した。!!"]]):-currentLevel(L),4<=L,L<=5.
-next([saiki_zenka,saiki_fibo],[[msg,"今度は難しい方にも挑戦してみましょう。"]]):-currentLevel(L),1<=L,L<=3.
-next([saiki_fibo,quick],[[msg,"ここからは応用問題です。自身のある人は挑戦して下さい。"]]).
+next([saiki_zenka,saiki_fibo],[[msg,"難易度の高い方の教材を選択しています。えらいですね。"]]):-currentLevel(L),4<=L,L<=5.
+next([saiki_zenka,saiki_fibo],[[msg,"自信があれば難易度の高い方にも挑戦してみましょう。"]]):-currentLevel(L),1<=L,L<=3.
+next([saiki_fibo,quick],[[msg,"ここからは応用問題です。自信のある人は挑戦してみて下さい。"]]).
 next([quick,hanoi],[]).
 next([hanoi,saiki_test],[]).
-next([saiki_test,saiki_intro],[[msg,"理解がいま一つのようです。最初からやり直してください。"]]):-testCompare(test_saiki,Point),Point<60.
-next([saiki_test,end],[[msg,"合格です。次は満点を目指しましょう。"]]):-testCompare(test_saiki,Point),60<=Point,Point<80.
 next([saiki_test,end],[[msg,"パーフェクトです。大変よく頑張りました。"]]):-testCompare(test_saiki,Point),80<=Point.
+next([saiki_test,end],[[msg,"合格です。次は満点を目指しましょう。"]]):-testCompare(test_saiki,Point),60<=Point,Point<80.
+next([saiki_test,saiki_intro],[[changeLv,1],[msg,"理解がいま一つのようです。最初からやり直してください。"]]):-testCompare(test_saiki,Point),Point<60.
 
 toc(saiki_intro,[]).
 toc(saiki_zenka,[]).
