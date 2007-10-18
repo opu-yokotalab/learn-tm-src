2130af1d2fa9037508cbcc112acc0351fea12c10
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-10-18 14:20:49 +0000

    微修正
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@25 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seqs/seq_saiki.pl
+++ seqs/seq_saiki.pl
@@ -1,7 +1,7 @@
 next([start,saiki_intro],[[msg,"再帰アルゴリズムについて学習しましょう。"]]).
 next([saiki_intro,saiki_zenka],[]).
-next([saiki_zenka,saiki_fibo],[[msg,"今度は難しい方にも挑戦してみましょう。"]]):-currentLevel(L),1<=L,L<=3.
 next([saiki_zenka,saiki_fibo],[[msg,"難しい方の教材によくぞ挑戦しました。感動した。!!"]]):-currentLevel(L),4<=L,L<=5.
+next([saiki_zenka,saiki_fibo],[[msg,"今度は難しい方にも挑戦してみましょう。"]]):-currentLevel(L),1<=L,L<=3.
 next([saiki_fibo,quick],[[msg,"ここからは応用問題です。自身のある人は挑戦して下さい。"]]).
 next([quick,hanoi],[]).
 next([hanoi,end],[[msg,"お疲れ様でした!!"]]).
