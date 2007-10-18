bac8f6ffae5de802e5036b198d94079434e6e757
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-10-18 11:34:44 +0000

    ハノイの塔 教材追加 C言語教材　画像の移動
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@22 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seqs/seq_saiki.pl
+++ seqs/seq_saiki.pl
@@ -1,10 +1,12 @@
 next([start,saiki_intro],[[msg,"再帰アルゴリズムについて学習しましょう。"]]).
 next([saiki_intro,saiki_zenka],[]).
 next([saiki_zenka,saiki_fibo],[]).
-next([saiki_fibo,saiki_sample],[]).
-next([saiki_sample,end],[[msg,"お疲れ様!!"]]).
+next([saiki_fibo,quick],[]).
+next([quick,hanoi],[]).
+next([hanoi,end],[[msg,"お疲れ様!!"]]).
 
 toc(saiki_intro,[]).
 toc(saiki_zenka,[]).
 toc(saiki_fibo,[]).
-toc(saiki_sample,[]).
+toc(quick,[]).
+toc(hanoi,[]).
