53cab835fd3bd1bcd6c97a068b30234b1938ef20
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-11-26 16:26:23 +0000

    修正
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@40 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seqs/seq_sample.pl
+++ seqs/seq_sample.pl
@@ -2,15 +2,15 @@
 next([start,intro],[[msg,"じゃあ、がんばって勉強しなさいよ。"]]).
 next([intro,grammar],[]).
 next([grammar,example],[]).
-next([example,IFtest],[]).
-next([IFtest,end],[[msg,"満点ですね。よくできました。"]]):-testCompare(test_000,Point),80<=Point.
-next([IFtest,end],[[msg,"一応合格ですが、もっと上を目指しましょう。"]]):-testCompare(test_000,Point),60<=Point,Point<80.
-next([IFtest,intro],[[msg,"あんた、もっとしっかりせられえ。"]]):-testCompare(test_000,Point),Point < 60.
+next([example,iftest],[]).
+next([iftest,end],[[msg,"満点ですね。よくできました。"]]):-testCompare(test_000,Point),80<=Point.
+next([iftest,end],[[msg,"一応合格ですが、もっと上を目指しましょう。"]]):-testCompare(test_000,Point),60<=Point,Point<80.
+next([iftest,intro],[[msg,"あんた、もっとしっかりせられえ。"]]):-testCompare(test_000,Point),Point < 60.
 
 toc(intro,[]).
 toc(grammar,[]).
 toc(example,[]).
-toc(IFtest,[]).
+toc(iftest,[]).
 
 changeLv([up,5],[]):-currentLevel(L),L==1.
 changeLv([down,1],[]):-currentLevel(L),L==5.
