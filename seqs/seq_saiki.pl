bf39c713adaf87ee04cf9e8bf8fd1fd48e5bb9b0
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-12-05 04:22:10 +0000

    condition 仕様変更
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@44 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seqs/seq_saiki.pl
+++ seqs/seq_saiki.pl
@@ -1,13 +1,13 @@
 next([start,saiki_intro],[[msg,"再帰アルゴリズムについて学習しましょう。"]]).
 next([saiki_intro,saiki_zenka],[]).
-next([saiki_zenka,saiki_fibo],[[msg,"難易度の高い方の教材を選択しています。えらいですね。"]]):-currentLevel(L),4<=L,L<=5.
-next([saiki_zenka,saiki_fibo],[[msg,"自信があれば難易度の高い方にも挑戦してみましょう。"]]):-currentLevel(L),1<=L,L<=3.
+next([saiki_zenka,saiki_fibo],[[msg,"難易度の高い方の教材を選択しています。えらいですね。"]]):-getCurrentLevel(L),4<=L,L<=5.
+next([saiki_zenka,saiki_fibo],[[msg,"自信があれば難易度の高い方にも挑戦してみましょう。"]]):-getCurrentLevel(L),1<=L,L<=3.
 next([saiki_fibo,quick],[[msg,"ここからは応用問題です。自信のある人は挑戦してみて下さい。"]]).
 next([quick,hanoi],[]).
 next([hanoi,saiki_test],[]).
-next([saiki_test,end],[[msg,"パーフェクトです。大変よく頑張りました。"]]):-testCompare(test_saiki,Point),80<=Point.
-next([saiki_test,end],[[msg,"合格です。次は満点を目指しましょう。"]]):-testCompare(test_saiki,Point),60<=Point,Point<80.
-next([saiki_test,saiki_intro],[[changeLv,1],[msg,"理解がいま一つのようです。最初からやり直してください。"]]):-testCompare(test_saiki,Point),Point<60.
+next([saiki_test,end],[[msg,"パーフェクトです。大変よく頑張りました。"]]):-getTestPoint(test_saiki,Point),80<=Point.
+next([saiki_test,end],[[msg,"合格です。次は満点を目指しましょう。"]]):-getTestPoint(test_saiki,Point),60<=Point,Point<80.
+next([saiki_test,saiki_intro],[[changeLv,1],[msg,"理解がいま一つのようです。最初からやり直してください。"]]):-getTestPoint(test_saiki,Point),Point<60.
 
 toc(saiki_intro,[]).
 toc(saiki_zenka,[]).
@@ -16,5 +16,5 @@ toc(quick,[]).
 toc(hanoi,[]).
 toc(saiki_test,[]).
 
-changeLv([up,5],[]):-currentLevel(L),L==1.
-changeLv([down,1],[]):-currentLevel(L),L==5.
+changeLv([up,5],[]):-getCurrentLevel(L),L==1.
+changeLv([down,1],[]):-getCurrentLevel(L),L==5.
--- seqs/seq_sample.pl
+++ seqs/seq_sample.pl
@@ -3,14 +3,14 @@ next([start,intro],[[msg,"じゃあ、がんばって勉強しなさいよ。"]]).
 next([intro,grammar],[]).
 next([grammar,example],[]).
 next([example,iftest],[]).
-next([iftest,end],[[msg,"満点ですね。よくできました。"]]):-testCompare(test_000,Point),80<=Point.
-next([iftest,end],[[msg,"一応合格ですが、もっと上を目指しましょう。"]]):-testCompare(test_000,Point),60<=Point,Point<80.
-next([iftest,intro],[[msg,"あんた、もっとしっかりせられえ。"]]):-testCompare(test_000,Point),Point < 60.
+next([iftest,end],[[msg,"満点ですね。よくできました。"]]):-getTestPoint(test_000,Point),80<=Point.
+next([iftest,end],[[msg,"一応合格ですが、もっと上を目指しましょう。"]]):-getTestPoint(test_000,Point),60<=Point,Point<80.
+next([iftest,intro],[[msg,"あんた、もっとしっかりせられえ。"]]):-getTestPoint(test_000,Point),Point < 60.
 
 toc(intro,[]).
 toc(grammar,[]).
 toc(example,[]).
 toc(iftest,[]).
 
-changeLv([up,5],[]):-currentLevel(L),L==1.
-changeLv([down,1],[]):-currentLevel(L),L==5.
+changeLv([up,5],[]):-getCurrentLevel(L),L==1.
+changeLv([down,1],[]):-getCurrentLevel(L),L==5.
