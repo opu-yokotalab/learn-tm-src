32df576d89a350d197da27b0d8e69b169baaeaf0
learn <learn@139e5372-204a-47f5-a165-b1eaa1f9cd96>
2007-12-06 19:15:45 +0000

    テストモジュール　名前変更
    git-svn-id: file:///Users/kunishi/tmp/svnroot/tm_src/trunk@53 139e5372-204a-47f5-a165-b1eaa1f9cd96

--- seqs/structure.pl
+++ seqs/structure.pl
@@ -1,13 +1,16 @@
-next([start,struct1],[]).
+next([start,struct1],[[msg,"構造体とポインタについて復習しましょう。テストの合格ラインは満点を取ることです。<br>頑張ってくださいね。"]]).
 next([struct1,struct2],[]).
-next([struct2,struct3],[]).
-next([struct3,struct4],[]).
-next([struct4,end],[[msg,"お疲れ様でした。"]]).
+
+next([struct2,struct3],[[msg,"演習問題です。頑張って実装しましょう。"]]):-getCurrentLevel(L),L==1.
+next([struct2,struct3],[[msg,"このページは｢簡単な内容｣も用意されています。ヒントが欲しいときは利用してください。"]]):-getCurrentLevel(L),L==5.
+
+next([struct3,struct_exam1],[[msg,"第1のテストです。満点取れるように頑張りましょう。"]]).
+next([struct_exam1,struct3],[[msg,"満点を取るまで挑戦してください。"]]):-getTestPoint(my_string_exam,Point),100 != Point.
+next([struct_exam1,end],[[msg,"お疲れ様でした。"]]):-getTestPoint(my_string_exam,Point),100 == Point.
 
 toc(struct1,[]).
 toc(struct2,[]).
 toc(struct3,[]).
-toc(struct4,[]).
 
 changeLv([up,5] ,[] ):-getCurrentLevel(L),L==1.
 changeLv([down,1] ,[] ):-getCurrentLevel(L),L==5.
